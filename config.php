024be21cb097d9cf95c5fcdc02551c899b067946
jerry54604 <i_54604@hotmail.com>
2016-04-24 22:04:46 +0800

    Remove cors

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
-
+/*
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+*/
 class Config {
 
 public static $servername = "localhost";
